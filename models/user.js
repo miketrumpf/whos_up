f033c0ddf335503b7f1b6671f1e7c96fdb09257e
Michael Trumpf <miketrumpf@gmail.com>
2015-04-17 13:45:55 -0400

    models created.  server setup.  need to do associations and migration tables

---
+++ models/user.js
@@ -0,0 +1,34 @@
+"use strict";
+module.exports = function(sequelize, DataTypes) {
+  var users = sequelize.define("users", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    facebookId : {
+      type: DataTypes.Integer,
+      allowNull: false
+      validate: {
+        isInt: {msg: "must be an integer"}
+        }
+      },
+      picture: {
+        type: DataTypes.STRING,
+      },
+      friends: {
+        type: DataTypes.INTEGER
+      }
+    }, {
+      underscored: true,
+
+      classMethods: {
+        associate: function(models) {
+
+        }
+      }
+    });
+    return users;
+};
+
+
+
