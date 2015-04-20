c7b20be0401e52544efd51af3a3b87e0f2922210
Michael Trumpf <miketrumpf@gmail.com>
2015-04-20 19:47:56 -0400

    made skeleton of route for creating fun post. Made create post template in index.html

--- public/js/views/usersView.js
+++ public/js/views/usersView.js
@@ -11,18 +11,14 @@ App.Views.UsersView = Backbone.View.extend({
    saveUser: function() {
     console.log("REACHED SAVE USER FUNCTION")
     var user = this.collection.toJSON();
-
-    // $.post("/users", {
-    //   var name = user.name;
-    //   var facebook_id = user.facebook_id;
-    //   var picture = user.picture;
-    // }).done(response)
+debugger
+ 
     
     // //app.user has the info I need already.  Do I need to pass it anything?
     // App.user = new App.Models.User
 
-    // App.users.save(({name: user.name, facebook_id: user.facebook_id, picture: user.picture}))
-    debugger
+    App.user.save(({name: user.name, facebook_id: user.facebook_id, picture: user.picture}))
+    
 
   }
 })
--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var models     = require("./models");
 
 //Models
 var User            = models.users;
-var FoodPosts       = models.foodPosts;
-var FunPosts        = models.funPosts;
-var DiscussionPosts = models.discussionPosts;
+var FoodPost       = models.foodPosts;
+var FunPost        = models.funPosts;
+var DiscussionPost = models.discussionPosts;
 
 //Express
 var app = express();
@@ -30,7 +30,7 @@ app.get("/users", function (req, res) {
       res.send(users)
     })
 });
-//Create user
+//Create user. route not being reached
 app.post("/users", function (req, res) {
   debugger
   var user = req.body;
@@ -42,6 +42,31 @@ app.post("/users", function (req, res) {
     });
 });
 
+//Fun Post Routes
+//create fun post
+app.post("/fun_posts", function (req, res) {
+  var user_name = req.user_name;
+  var title = req.title;
+  var genre = req.genre;
+  var content =req.content;
+  var start_date= req.start_date;
+  var exp_date = req.exp_date;
+  var facebook_id = req.facebook_id;
+  var user_id = req.user_id;
+
+  FunPost.create({
+    title: title,
+    genre: genre,
+    content: content,
+    start_date: start_date,
+    exp_date: exp_date,
+    facebook_id: facebook_id,
+    user_id: user_id
+  })
+    .then(function(newPost) {
+      res.send(newPost)
+    })
+});
 
 
 app.listen(3000, function() {
